abad6237e62b69be5bb049550c07d9aeda0f8938
Chao Xie <chaoranxie@gmail.com>
2017-05-09 16:42:43 -0400

    backup firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCjpWgvS0AFvuvLxueg2QuDTruLhadHZxE",
+    authDomain: "climb-with-me.firebaseapp.com",
+    databaseURL: "https://climb-with-me.firebaseio.com",
+    projectId: "climb-with-me",
+    storageBucket: "climb-with-me.appspot.com",
+    messagingSenderId: "484468721914"
+  }
 };
