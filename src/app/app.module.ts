23856a8aa71d46f9b7146410326425a2e02ae034
Chao Xie <chaoranxie@gmail.com>
2017-05-16 19:42:12 -0400

    add material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// import { MdButtonModule, MdCheckboxModule, MdIconModule, MdChipsModule} from '@angular/material';
+import { MaterialModule } from '@angular/material';
+import { MdDialogModule } from '@angular/material';
+
+// MaterialModul
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -10,6 +18,8 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { ClimbListComponent } from './climb-list/climb-list.component';
 
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +27,10 @@ import { ClimbListComponent } from './climb-list/climb-list.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MdDialogModule,
+    // MdButtonModule, MdCheckboxModule, MdIconModule, MdChipsModule,
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase),
