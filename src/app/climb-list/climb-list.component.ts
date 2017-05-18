2e4a2555896817584251ca8a5fea4505f63a3d10
Chao Xie <chaoranxie@gmail.com>
2017-05-18 00:58:25 -0400

    test

--- src/app/climb-list/climb-list.component.ts
+++ src/app/climb-list/climb-list.component.ts
@@ -13,6 +13,30 @@ export class ClimbListComponent implements OnInit {
   queryList: FirebaseListObservable<any[]>;
   colorSubject: Subject<any>;
 
+  folders = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  notes = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
   constructor(db: AngularFireDatabase) {
     this.colorSubject = new Subject();
     this.items = db.list('/items', {
