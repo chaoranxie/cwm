595fcf2ce119d3d668d6664d5c334593636be908
Chao Xie <chaoranxie@gmail.com>
2017-05-18 10:34:10 -0400

    disable pwa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCjpWgvS0AFvuvLxueg2QuDTruLhadHZxE",
-    authDomain: "climb-with-me.firebaseapp.com",
-    databaseURL: "https://climb-with-me.firebaseio.com",
-    projectId: "climb-with-me",
-    storageBucket: "climb-with-me.appspot.com",
-    messagingSenderId: "484468721914"
+    apiKey: 'AIzaSyCjpWgvS0AFvuvLxueg2QuDTruLhadHZxE',
+    authDomain: 'climb-with-me.firebaseapp.com',
+    databaseURL: 'https://climb-with-me.firebaseio.com',
+    projectId: 'climb-with-me',
+    storageBucket: 'climb-with-me.appspot.com',
+    messagingSenderId: '484468721914'
   }
 };
