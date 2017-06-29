22c640b5cb41b68066a8d0820c30c233cc140bdc
Chao Xie <chaoranxie@gmail.com>
2017-06-29 12:25:44 -0400

    backup:x

--- src/app/route-detail/route-detail.component.ts
+++ src/app/route-detail/route-detail.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable, Subscription} from 'rxjs/Rx';
+import { AppStore } from '../store/app-store';
+import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/filter';
 
+import { Route } from '../model';
+
+//
+// Show detail information about Route
+// Show all the comments on this route
+// Allow people to add additional comments
 @Component({
   selector: 'app-route-detail',
   templateUrl: './route-detail.component.html',
@@ -11,19 +20,51 @@ export class RouteDetailComponent implements OnInit {
 
   routeSub: Subscription;
   id: string;
-
+  route: Route;
+  public routes;
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private store: Store<AppStore>,
   ) { }
 
   ngOnInit() {
-    // debugger;
+
+          // Once you have the id,
+          // Check local store and if exists return it
+
+          // If not,
+            // make call to get the data which will save it in store.
+            // then, use the store
+    this.routes = this.store.select('routes');
+
     this.routeSub = this.activatedRoute.params.subscribe(params => {
       this.id = params['id'];
+
+      // this.attending = this.people.map(p => p.filter(person => person.attending));
+
+      this.routes.map(r => r.filter(route => route.$key===this.id)).subscribe(result => {
+        this.route= result[0];
+      })
+      // this.store.select('routes').filter(route => route.$key===this.id).subscribe(routes => {
+      //   debugger;
+      // });
+      // })
+
+// .filter(route => route.$key==='-KkreHSpGzs4cec4lojf')
+      //   }).take(1).subscribe(routes => {
+      //   debugger;
+      //   this.route=routes[0];
+      // });
+
     } );
   }
 
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+  }
 
-
+  hasCompleted(routeKey: string): boolean {
+    return false;
+  }
 }
